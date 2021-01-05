67444566f1f40129c8a14cdf1acab51a52aa11c0
Nicolas1st <nicolashereigo@gmail.com>
2021-01-05 23:10:34 +0300

    Made the navbar responsive

---
+++ static/js/index.js
@@ -0,0 +1,19 @@
+const panel = document.querySelector(".sandwich__panel");
+const sandwich = document.querySelector(".sandwich");
+const sandwichLinks = document.querySelector(".sandwich__links");
+
+
+sandwich.addEventListener('click', () => {
+    // panel.style.display = 'block';
+    // sandwichLinks.style.display = 'flex';
+    // sandwichLinks.style.height= 'fit-content';
+    // sandwichLinks.style.width = '74vw';
+    // sandwichLinks.style.padding = '25px';
+    // sandwichLinks.style.opacity = '1';
+    if (sandwichLinks.style.display == 'flex') {
+        sandwichLinks.style.display = 'none';
+    } else {
+        sandwichLinks.style.display = 'flex';
+    }
+    
+})
